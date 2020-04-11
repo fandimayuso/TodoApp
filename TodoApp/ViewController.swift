3b9c5856819f77767d0311dcf72903a7d28f7141
Muhammat Fandi Mayuso <>
2020-04-11 15:55:14 +0700

    Initial project

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Muhammat Fandi Mayuso on 11/4/20.
+//  Copyright © 2020 Muhammat Fandi Mayuso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
